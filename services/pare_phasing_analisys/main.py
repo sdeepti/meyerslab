3172f35180833e67c492f6e49a1ea670eb465735
Walter Moreira <walter@waltermoreira.net>
2015-05-21 11:32:52 -0500

    Add short example for another service

---
+++ services/pare_phasing_analisys/main.py
@@ -0,0 +1,18 @@
+import json
+
+import services.common.tools
+
+
+def search(args):
+    chrnum = args['chrnum']
+    start = args['start']
+    # default strand: w
+    strand = args.get('strand', 'w')
+
+    data = services.common.tools.do_request(
+        'at_pare', 'PAinfo.php', chrnum=chrnum, win_beg=start, strand=strand)
+    services.common.tools.send(data['phasing_analysis'])
+
+
+def list(args):
+    raise Exception('not implemented yet')
