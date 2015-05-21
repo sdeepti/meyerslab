87e1cbeec642de8b6c4cf073dbdc902f7b8cd032
Walter Moreira <walter@waltermoreira.net>
2015-05-21 11:11:19 -0500

    Implement abundances search

--- services/abundances/main.py
+++ services/abundances/main.py
@@ -4,5 +4,17 @@ import services.common.tools
 
 
 def search(args):
-    print json.dumps({'x': services.common.tools.foo()})
+    # 'gene' parameter is guarantee to be there because we requested
+    # validation by Adama and we declared it to be mandatory
+    gene = args['gene']
+    # 'model' parameter was declared optional
+    # Assume value '1' if not given.
+    model = args.get('model', '1')
 
+    data = services.common.tools.do_request(
+        'at_sRNA', 'abundances.php', featureName=gene, model=model)
+    services.common.tools.send(data['abundance_data'])
+
+
+def list(args):
+    raise Exception('not implemented yet')
