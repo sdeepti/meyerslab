9eac900a7814ae2d0139b55e887d793dd586a5e9
Walter Moreira <walter@waltermoreira.net>
2015-05-21 11:10:15 -0500

    Add common way of doing request and returning data

--- services/common/tools.py
+++ services/common/tools.py
@@ -1,4 +1,38 @@
+import json
+import urlparse
 
-def foo():
-    return 5
+import requests
 
+
+MEYERS_BASE_URL = 'https://mpss.udel.edu/web/php/pages/'
+
+
+def do_request(site, endpoint, **kwargs):
+    """Perform a request to SITE and return JSON."""
+
+    url = urlparse.urljoin(MEYERS_BASE_URL, endpoint)
+    kwargs['SITE'] = site
+    kwargs['format'] = 'json'
+    response = requests.get(url, verify=False, params=kwargs)
+
+    # Raise exception and abort if requests is not successful
+    response.raise_for_status()
+
+    try:
+        # Try to convert result to JSON
+        # abort if not possible
+        return response.json()
+    except ValueError:
+        raise Exception('not a JSON object: {}'.format(response.text))
+
+
+def send(data):
+    """Display `data` in the format required by Adama.
+
+    :type data: list
+
+    """
+
+    for elt in data:
+        print json.dumps(elt)
+        print '---'
