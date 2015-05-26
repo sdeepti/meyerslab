ed37d0f3221e09a404ae54bb360da6f5ebc354fd
Deepti <sdeepti@udel.edu>
2015-05-26 15:53:26 -0400

    yml name

---
+++ services/sRNA_phasing_window/main.py
@@ -0,0 +1,19 @@
+import json
+
+import services.common.tools
+
+def search(args):
+    chrnum = args['chrnum']
+    start = args['start']
+    # default strand: w
+    strand = args.get('strand', 'w')
+
+    data = services.common.tools.do_request(
+        'at_sRNA', 'PAinfo.php', list='phasing_window',
+        chrnum=chrnum, win_beg=start, strand=strand)
+
+    services.common.tools.send(data['phasing_window'])
+
+
+def list(args):
+    raise Exception('not implemented yet')
