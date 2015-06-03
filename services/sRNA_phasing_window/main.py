a76b92bdeb5850c54d80a7aa56e79bd2a457bd6a
Deepti <sdeepti@udel.edu>
2015-06-02 23:23:37 -0400

    Fix the window adapter

--- services/sRNA_phasing_window/main.py
+++ services/sRNA_phasing_window/main.py
@@ -12,7 +12,7 @@ def search(args):
         'at_sRNA', 'PAinfo.php', list='phasing_window',
         chrnum=chrnum, win_beg=start, strand=strand)
 
-    services.common.tools.send(data['phasing_analysis'])
+    services.common.tools.send(data['phasing_window'])
 
 def list(args):
     raise Exception('not implemented yet')
