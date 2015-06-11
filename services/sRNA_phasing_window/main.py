621446b37f8fa385eefeb451e30d09ebeacaa3d1
Deepti <sdeepti@udel.edu>
2015-06-11 09:17:08 -0400

    chromosome

--- services/sRNA_phasing_window/main.py
+++ services/sRNA_phasing_window/main.py
@@ -8,9 +8,18 @@ def search(args):
     strand = args['strand']
     phase_len = args.get('phase_len', 21)
 
+    words = chromosome.split();
+
+    if words[1] == 'M':
+       chrnum = 6
+    elif words[1] == 'C':
+       chrnum = 7
+    else:
+       chrnum = words[1]
+  
     data = services.common.tools.do_request(
         'at_sRNA', 'PAinfo.php', list='phasing_window',
-        chrnum=chromosome, win_beg=start, strand=strand, phase_len=phase_len)
+        chrnum=chrnum, win_beg=start, strand=strand, phase_len=phase_len)
 
     services.common.tools.send(data['phasing_window'])
 
