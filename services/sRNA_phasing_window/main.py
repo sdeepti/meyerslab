e18b2da34fac361328144af837c47bdb0af22e0b
Deepti <sdeepti@udel.edu>
2015-06-11 00:02:28 -0400

    chromosome

--- services/sRNA_phasing_window/main.py
+++ services/sRNA_phasing_window/main.py
@@ -3,14 +3,14 @@ import json
 import services.common.tools
 
 def search(args):
-    chrnum = args['chrnum']
+    chromosome = args['chromosome']
     start = args['start']
     strand = args['strand']
     phase_len = args.get('phase_len', 21)
 
     data = services.common.tools.do_request(
         'at_sRNA', 'PAinfo.php', list='phasing_window',
-        chrnum=chrnum, win_beg=start, strand=strand, phase_len=phase_len)
+        chrnum=chromosome, win_beg=start, strand=strand, phase_len=phase_len)
 
     services.common.tools.send(data['phasing_window'])
 
