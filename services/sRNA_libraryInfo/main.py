02dffd7719946336f853c10bbb4aea4e7abf71c9
Deepti <sdeepti@udel.edu>
2015-07-07 17:42:00 -0400

    added list

--- services/sRNA_libraryInfo/main.py
+++ services/sRNA_libraryInfo/main.py
@@ -4,7 +4,8 @@ import services.common.tools
 
 def search(args):
     data = services.common.tools.do_request(
-        'at_sRNA', 'library_info.php')
+        'at_sRNA', 'library_info.php', list='library_info')
+
     services.common.tools.send(data['library_info'])
 
 def list(args):
