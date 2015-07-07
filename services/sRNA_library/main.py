e1cd760a3a672f0b9620b8c07e78dda689634c1a
Deepti <sdeepti@udel.edu>
2015-07-07 10:45:26 -0400

    library info

--- services/sRNA_library/main.py
+++ services/sRNA_library/main.py
@@ -6,7 +6,7 @@ def search(args):
     data = services.common.tools.do_request(
         'at_sRNA', 'library_info.php')
 
-    services.common.tools.send(data['chromosome'])
+    services.common.tools.send(data['library'])
 
 def list(args):
     raise Exception('not implemented yet')
